565e5bcccf78b68d8a413cc2ee3f5228ec724aff
momokoueng <3395491319@qq.com>
2025-10-30 22:33:41 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let index = 0;
-const slides = document.querySelectorAll(".banner img");
-
-function showSlide() {
-  slides.forEach((img, i) => img.classList.toggle("active", i === index));
-  index = (index + 1) % slides.length;
-}
-
-setInterval(showSlide, 4000);
